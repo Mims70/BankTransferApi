f3175313a1e8e7658f6e263a44b0e9148cb82b25
mims70 <anyiammiracle7777@gmail.com>
2026-03-09 16:46:28 +0100

    added Scalar ui and modified Delete endpoint to be role based

--- Program.cs
+++ Program.cs
@@ -3,16 +3,23 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using PaymentAPI.Data;
+using Scalar.AspNetCore;
 using PaymentAPI.Services;
-using Microsoft.EntityFrameworkCore.SqlServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+// Swagger - .NET 10 already has OpenAPI support built-in
+builder.Services.AddOpenApi();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-    builder.Services.AddHttpClient<AccountVerificationService>();
 
+// Register services
+builder.Services.AddHttpClient<AccountVerificationService>();
+
+// JWT Authentication
 var jwtKey = builder.Configuration["Jwt:Key"];
 var key = Encoding.ASCII.GetBytes(jwtKey);
 
@@ -39,8 +46,12 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
+// Enable OpenAPI/Swagger
+app.MapOpenApi();
+app.MapScalarApiReference();
+
 app.UseHttpsRedirection();
-app.UseAuthentication();  
-app.UseAuthorization();  
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
